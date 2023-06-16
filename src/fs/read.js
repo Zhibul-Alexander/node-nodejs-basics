c0d0a5be4d7a25f06412cbb87be0a41bb1948542
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-06-16 18:21:34 +0400

    feat: add solution fs/read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import {join} from 'path';
+import {existsSync} from 'fs';
+import {readFile} from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const folderPath = './src/fs/files';
+    const fileName = 'fileToRead.txt';
+    const filePath = join(folderPath, fileName);
+
+    if (!existsSync(filePath)) {
+        return console.log('FS operation failed')
+    }
+
+    try {
+        const readFileResult = await readFile(filePath, 'utf-8');
+        console.log(readFileResult)
+    } catch {
+        console.log('Error reading file');
+    }
 };
 
 await read();
