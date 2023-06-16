8d5492c32897beeccec7a745270197d50d5a0d5c
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-06-16 14:24:27 +0400

    feat: add solution fs/copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import {existsSync} from 'fs';
+import {mkdir, readdir, copyFile} from 'fs/promises';
+import {join} from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const folderPath = './src/fs/files';
+    const copyFolderPath = './src/fs/files_copy';
+
+    if (!existsSync(folderPath) || existsSync(copyFolderPath)) {
+        return console.log('FS operation failed')
+    }
+
+    try {
+        await mkdir(copyFolderPath);
+        const readdirSyncResult = await readdir(folderPath);
+
+        for (const file of readdirSyncResult) {
+            const filePath = join(folderPath, file);
+            const copyFilePath = join(copyFolderPath, file);
+            await copyFile(filePath, copyFilePath);
+        }
+
+        console.log('Folder copied');
+    } catch {
+        console.log('Error copying folder');
+    }
 };
 
 await copy();
