5fd3127c1bc1ec9a1dfd902e6e2f0e3eda4a0a5a
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-06-16 18:41:52 +0400

    feat: add solution cli/env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
-const parseEnv = () => {
-    // Write your code here 
+export const parseEnv = () => {
+    const prefix = 'RSS_'
+    const processEnv = process.env
+
+    const envKeys = Object.keys(processEnv);
+    const rssKeys = envKeys.filter((key) => {
+        return key.startsWith(prefix)
+    })
+
+    if (!rssKeys.length) {
+        return console.log('No RSS_ environment variables found')
+    }
+
+    rssKeys.forEach((key) => {
+         console.log(`${key}=${processEnv[key]}`)
+    });
 };
 
 parseEnv();
