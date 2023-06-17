8d3ea29e50b12b04ef3ad9736c2915fe125b35c9
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-06-18 00:26:00 +0400

    feat: add solution streams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import {join} from 'path';
+import {createReadStream} from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const folderPath = './src/streams/files';
+    const fileName = 'fileToRead.txt';
+    const filePath = join(folderPath,fileName)
+
+    await new Promise(() => {
+        const fileStream = createReadStream(filePath, {encoding: 'utf8'});
+
+        fileStream.on('data', (data) => {
+            process.stdout.write(data);
+        })
+    })
 };
 
 await read();
